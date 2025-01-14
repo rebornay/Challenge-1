11fcecfb171f1af28d09f67696a4b025356fc272
Dauda Samson Ayomide <daudasamson2214@gmail.com>
2025-01-14 02:15:20 +0100

    Boolean variable

--- script.js
+++ script.js
@@ -12,11 +12,15 @@ const weightOfJohn = 92;
 
 
 // MARK'S BMI
-const markBMI = 78 / 1.69 ** 2;
+const markBMI = 78 / 1.69 ** 2;  //First formula
 //const markBMI = 78 / (1.69 * 1.69); // Second formula
 console.log(markBMI);
 
 // JOHN'S BMI 
-const johnBMI = 92 / 1.95 ** 2; 
+const johnBMI = 92 / 1.95 ** 2;  // First formula
 //const johnBMI = 92 / (1.95 * 1.95); // Second formula 
 console.log(johnBMI);
+
+// Boolean Variable
+const markHigherBMI = markBMI > johnBMI; 
+console.log(markHigherBMI);
