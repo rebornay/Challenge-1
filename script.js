c17630c1c78ee0cf545879ae389d002aa135ad6d
Dauda Samson Ayomide <daudasamson2214@gmail.com>
2025-01-14 02:34:10 +0100

    Changed the numbers to variables

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ const weightOfJohn = 92;
 
 
 // MARK'S BMI
-const markBMI = 78 / 1.69 ** 2;  //First formula
+const markBMI = weightOfMark / heightOfMark ** 2;  //First formula
 //const markBMI = 78 / (1.69 * 1.69); // Second formula
 console.log(markBMI);
 
 // JOHN'S BMI 
-const johnBMI = 92 / 1.95 ** 2;  // First formula
+const johnBMI = weightOfJohn / heightOfJohn ** 2;  // First formula
 //const johnBMI = 92 / (1.95 * 1.95); // Second formula 
 console.log(johnBMI);
 
@@ -42,15 +42,15 @@ const weightOfJohn2 = 85;
 
 
 // MARK'S BMI
-const markBMI2 = 95 / 1.88 ** 2;  //First formula
+const markBMI2 = weightOfMark2 / heightOfMark2 ** 2;  //First formula
 //const markBMI2 = 78 / (1.69 * 1.69); // Second formula
 console.log(markBMI2);
 
 // JOHN'S BMI 
-const johnBMI2 =85 / 1.76 ** 2;  // First formula
+const johnBMI2 = weightOfJohn2 / heightOfJohn2 ** 2;  // First formula
 //const johnBMI2 = 92 / (1.95 * 1.95); // Second formula 
 console.log(johnBMI2);
 
 // Boolean Variable
-const markHigherBMI2 = markBMI > johnBMI; 
+const markHigherBMI2 = markBMI2 > johnBMI2; 
 console.log(markHigherBMI2);
