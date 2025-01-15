12542da2d4cdf49b6993666ad341e2a2ead70ad3
Dauda Samson Ayomide <daudasamson2214@gmail.com>
2025-01-15 23:48:52 +0100

    Console logging outside if/else block

--- script.js
+++ script.js
@@ -85,7 +85,10 @@ if (isOldEnough) {
 }
 
 const birthYear = 1998;
-
+let century;
 if (birthYear <= 2000) {
- let century = 20
+ century = 20;
+} else {
+    century = 21;
 }
+console.log(century);
