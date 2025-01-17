9dd2eb693150effd84719f202390dc3ed59b895f
Dauda Samson Ayomide <daudasamson2214@gmail.com>
2025-01-17 22:48:12 +0100

    Converted string to number

--- script.js
+++ script.js
@@ -111,5 +111,5 @@ console.log(century);
 } */
 
 const inputYear = '1991';
-console.log(Number(inputYear));
-console.log(inputYear + 18);
+console.log(Number(inputYear),  inputYear);
+console.log(Number(inputYear) + 18);
