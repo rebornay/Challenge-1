f5efb5736f7586414c21671ef3fc7f3ae6abb514
Dauda Samson Ayomide <daudasamson2214@gmail.com>
2025-01-22 23:54:50 +0100

    30

--- script.js
+++ script.js
@@ -172,4 +172,5 @@ if (favourite === 23){
 }
 
 if (favourite !== 23) console.log("Why not 23?")
-if (favourite !== 25) console.log("Why not 23?")
+if (favourite !== 25) console.log("Why not 25?")
+if (favourite !== 30) console.log("Why not 30?")
