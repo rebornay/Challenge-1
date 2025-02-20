e576bea8037d564a06c1d17db2191c5cf17ec9a2
Dauda Samson Ayomide <daudasamson2214@gmail.com>
2025-02-20 18:08:53 +0100

    Created a const variable

--- script.js
+++ script.js
@@ -317,8 +317,7 @@ console.log(`I like to drink ${age >= 18 ? 'Wine': 'Vijumilk'}`);
 
 // Building a simple tip calculator
 
-const bill = 430;
-let tip;
-bill >= 50 && bill <= 300 ? tip = (bill * 0.15): tip = (bill * 0.2);
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15: bill * 0.2;
 
 console.log(`The bill value was ${bill} the tip was ${tip}, and the total value ${bill + tip}`);
